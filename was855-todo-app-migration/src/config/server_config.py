f09219a2618f773d5bcde16f97a297ea028f7caa
keunlee <kelee@redhat.com>
2021-12-03 17:06:34 -0600

    update server jython script to match as closely to pristine copy

--- was855-todo-app-migration/src/config/server_config.py
+++ was855-todo-app-migration/src/config/server_config.py
@@ -10,9 +10,10 @@ NodeName=AdminControl.getNode()
 # The following variables are used to replace sensitive data in the configuration for the application.
 # The values for these variables were not collected because the includeSensitiveData option was not specified.
 # ============================================================
+ocp_project_namespace='demo-todo-was855-custom'
 pg_datasource_password_1='postgres'
 pg_datasource_user_1='postgres'
-pg_datasource_host_1='postgres.demo-todo-was855-custom'
+pg_datasource_host_1='postgres.' + ocp_project_namespace
 # ============================================================
 
 print 'Starting Creating JVM Properties'
