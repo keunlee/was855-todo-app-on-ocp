ed56eba389cd576514cbbb3a9c3d846270f4b9ec
keunlee <kelee@redhat.com>
2021-12-03 16:43:34 -0600

    update database create script

--- was855-todo-app-migration/scripts/01-database.sql
+++ was855-todo-app-migration/scripts/01-database.sql
@@ -4,7 +4,5 @@ CREATE DATABASE todo
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF-8'
-    LC_CTYPE = 'en_US.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
